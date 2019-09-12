bdbb349f38b37b6a93e54592cfd47a0fed7fb360
PsyanticY <iuns@outlook.fr>
2019-09-12 13:22:23 +0100

    go tutorials

---
+++ go/web-server/README.md
@@ -0,0 +1,1 @@
+Full tutorial here: https://pythonprogramming.net/go/concurrency-web-app-go-language-programming-tutorial/
---
+++ go/web-server/web.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+type NewsMap struct {
+	Keyword  string
+	Location string
+}
+
+type NewsAggPage struct {
+	Title string
+	News  map[string]NewsMap
+}
+
+type Sitemapindex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
+}
+
+func newsRoutine(c chan News, Location string) {
+	defer wg.Done()
+	var n News
+	Location = strings.TrimSpace(Location)
+	resp, _ := http.Get(Location)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &n)
+	resp.Body.Close()
+	c <- n
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+
+	var s Sitemapindex
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+	news_map := make(map[string]NewsMap)
+	resp.Body.Close()
+	queue := make(chan News, 30)
+
+	for _, Location := range s.Locations {
+		wg.Add(1)
+		go newsRoutine(queue, Location)
+	}
+	wg.Wait()
+	close(queue)
+
+	for elem := range queue {
+		for idx, _ := range elem.Keywords {
+			news_map[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
+		}
+	}
+
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+
+	t, _ := template.ParseFiles("newsaggtemplate.html")
+	t.Execute(w, p)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
+	http.ListenAndServe(":8000", nil)
+}
